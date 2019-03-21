2d546ad365237a653cca654dec4d3c02a11b1df2
MALDIN <mohamad-aez.aldin@capgemini.com>
2019-03-21 14:37:29 +0100

    creat new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SeriesComponent } from './series/series.component';
+import { MoviesComponent } from './movies/movies.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MoviesComponent, SeriesComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/models/movie.model.ts
@@ -0,0 +1,7 @@
+/** A movie object */
+export class Movie {
+  public title: string;
+  public director: string;
+  public year: number;
+  public urlID: string;
+}
